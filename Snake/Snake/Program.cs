fa84ec3d0c5ea784838ab1384c5601adfba15cce
Artem <icq11007170@gmail.com>
2020-10-12 21:31:27 +0300

    Функция Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 2;
+            int y1 = 3;
+            Char sym1 = '&';
+
+            Draw(x1, y1, sym1);
+
             Console.ReadLine();
         }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
     }
 }
